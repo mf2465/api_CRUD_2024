f78e2da32bce73a902d6761d44a9c90bb85b788c
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2024-06-26 09:00:48 -0300

    Add files via upload

--- pages/footer.php
+++ pages/footer.php
@@ -1,31 +1,27 @@
- <!--  inicio Footer -->
+<!--  inicio Footer -->
 
- <footer>
+<footer>
   <nav class="navbar navbar-expand-lg navbar-dark text-white justify-content-center" style="background-color: #7ECEC3;">
   
       <ul class="navbar-nav" style="justify-content: space-evenly; width: 100%">
      
       <li class="nav-item active">
-        <a class="nav-link" href="#">Preguntas <br>frecuentes</a>
+        <a class="nav-link text-white-50" href="#">Link 1</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Contáctanos</a>
+        <a class="nav-link text-white-50" href="#">Link 2</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Prensa</a>
+        <a class="nav-link text-white-50" href="#">Link 3</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Conferencias</a>
+        <a class="nav-link text-white-50" href="#">Link 4</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Términos y<br>condiciones</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Privacidad</a>
+        <a class="nav-link text-white-50" href="#">Link 5</a>
       </li>
     </ul>
     
   </nav>  
 </footer>
-
- <!--  fin Footer -->
+<!--  fin Footer -->
