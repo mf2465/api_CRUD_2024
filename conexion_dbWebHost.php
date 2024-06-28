ab6a914c8292ecea00b656051c2c13e51421ec74
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2024-06-28 08:43:52 -0300

    Delete conexion_dbWebHost.php
    fue utilizado como borrador para tomar los datos de la conexión

--- conexion_dbWebHost.php
+++
@@ -1,7 +0,0 @@
-	//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	
-	$host="localhost";
-	$user="id22362038_apicrud2024";
-	$pass="Costamar393";
-	$DB="id22362038_apicrud2024";
-	$tabla1 = "items";
