3e991193a0aa66b2a8fa2bc666173880d9e69644
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2025-08-30 15:47:25 -0300

    Update conexion.php

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,10 +1,11 @@
 <?php
 	
-	$mysqli=new mysqli("localhost","id22362038_rest_api_demo","Costamar393$","id22362038_rest_api_demo"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli=new mysqli("localhost","id22362038_rest_api_demo","","id22362038_rest_api_demo"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 	}
 	
+
 ?>
